10374f9327c8f34290b6acc9fee613fc1fdbe995
ElExca <emili17_2002@hotmail.com>
2024-04-09 22:48:38 -0600

    add notifications

--- application/usecases/list_notifications.py
+++
@@ -1,6 +0,0 @@
-class ListNotifications:
-    def __init__(self, notification_repository):
-        self.notification_repository = notification_repository
-
-    def execute(self, user_id):
-        return self.notification_repository.find_by_user_id(user_id)
--- domain/validations/notification_validations.py
+++
@@ -1,5 +0,0 @@
-def validate_notification(notification):
-    if not notification.message:
-        raise ValueError("Notification message cannot be empty")
-    if notification.type not in ['info', 'alert', 'reminder']:
-        raise ValueError("Invalid notification type")
